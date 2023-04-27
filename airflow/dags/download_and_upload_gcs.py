8e41eb681d9b4b9e5db01db9e7fd56420d2f8f6a
undersea-nate <131403349+undersea-nate@users.noreply.github.com>
2023-04-27 00:20:01 -0400

    Updated comments

--- airflow/dags/download_and_upload_gcs.py
+++ airflow/dags/download_and_upload_gcs.py
@@ -31,23 +31,6 @@ if (int(year) == 2020 and int(month) >= 4) or (int(year) >= 2021): #raw data for
     format = "new"
 else:
     format = "old"
-#TO DO:
-#Create "master" dataset where I can join all the individual datasets into one big one
-    #Create a new task to drop current month's table BEFORE creating it
-    #drop rows from "master" dataset from current month (to ensure that data isn't entered twice)
-    #OPTIONAL: drop rows from current month's dataset that are not from the current time range
-    #insert current month's table into "master" dataset
-#use dbt core or spark (or both?) to transform the data - start and end dates especially need to be separated
-    #OPTIONAL: Also create morning commute and evening commute flags
-    #OPTIONAL: Can use Longitude and Latitude file as a "seed" in DBT (only 107 distinct values) 
-    #Should also trim the station names to remove spaces
-    #OPTIONAL: split into dev, stg, and prd
-        #dev - local place where you can mess around with data
-        #staging - as similar to production environment as possible
-        #production - where your live data is
-    #OPTIONAL: Testing in DBT - look up how to do this!
-#create dashboard in PBI, maybe 
-#create diagram here: https://www.diagrams.net/
 
 def format_to_parquet(src_file):
     if not src_file.endswith('.csv'):
@@ -56,9 +39,6 @@ def format_to_parquet(src_file):
     table = pv.read_csv(src_file)
     pq.write_table(table, src_file.replace('.csv', '.parquet'))
 
-
-
-###### def upload_file_to_gcs()
 def upload_to_gcs(bucket, object_name, local_file):
     """
     Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
@@ -78,13 +58,6 @@ def upload_to_gcs(bucket, object_name, local_file):
 
     blob = bucket.blob(object_name)
     blob.upload_from_filename(local_file)
-
-#def evaluate_dataset_format(year: str, month:str) -> None:
-#    if (int(year) == 2020 and int(month) >= 4) or (int(year) >= 2021):
-#        return "create_external_bq_table_new_format"
-#    else:
-#        return "create_external_bq_table_old_format"
-
 
 default_args = {
     "owner": "airflow",
@@ -113,7 +86,6 @@ with DAG(dag_id="download_and_upload_gcs",
         task_id="format_to_parquet_task",
         python_callable=format_to_parquet,
         op_kwargs={
-            #"src_file": f"{path_to_local_home}/{year}{month}-captialbikeshare-tripdata.csv",
             "src_file": f"{path_to_local_home}/{dataset_file}",
         },
     )
@@ -124,24 +96,8 @@ with DAG(dag_id="download_and_upload_gcs",
         op_kwargs={
             "bucket": BUCKET,
             "object_name": f"{format}/{dataset_parquet}",
-            #"local_file": f"{path_to_local_home}/{year}{month}-captialbikeshare-tripdata.parquet",
             "local_file": f"{path_to_local_home}/{dataset_parquet}",
         },
     )
 
-    #evaluate_dataset_format_task = BranchPythonOperator(
-    #    task_id = 'evaluate_dataset_format',
-    #    python_callable = evaluate_dataset_format,
-    #    op_kwargs={
-    #        'year': f"{year}",
-    #        'month': f"{month}"
-    #    }
-    #)
-
-
     download_dataset_task >> unzip_dataset_task >> format_to_parquet_task >> local_to_gcs_task
-
-    #download_dataset_task >> unzip_dataset_task >> upload_to_s3_task >> evaluate_dataset_format_task >> [create_redshift_table_new_format_task, create_redshift_table_old_format_task]
-    #drop_redshift_table_task >> evaluate_dataset_format_task
-    #create_redshift_table_old_format_task >> transfer_s3_to_redshift_old_format_task >> append_bike_data_old_format_task
-    #create_redshift_table_new_format_task >> transfer_s3_to_redshift_new_format_task >> append_bike_data_new_format_task
