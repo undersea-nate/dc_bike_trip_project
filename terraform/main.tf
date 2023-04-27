163689aa865be4e4a9b453a474d9b3a7cdfa59ea
undersea-nate <131403349+undersea-nate@users.noreply.github.com>
2023-04-27 12:38:12 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   required_version = ">= 1.0"
-  backend "local" {}  # Can change from "local" to "gcs" (for google) or "s3" (for aws), if you would like to preserve your tf-state online
+  backend "local" {} 
   required_providers {
     google = {
       source  = "hashicorp/google"
