73db1fc31f55ddfcadc3a39f6cc88a308c75a759
undersea-nate <131403349+undersea-nate@users.noreply.github.com>
2023-04-27 03:04:43 -0400

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+locals {
+  data_lake_bucket = "dc_bike_trip_data"
+}
+
+variable "project" {
+  description = "stately-transit-383518"
+}
+
+variable "region" {
+  description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
+  default = "us-east1"
+  type = string
+}
+
+variable "storage_class" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default = "STANDARD"
+}
+
+variable "BQ_DATASET" {
+  description = "BigQuery Dataset that raw data (from GCS) will be written to"
+  type = string
+  default = "bike_data_dev"
+}
