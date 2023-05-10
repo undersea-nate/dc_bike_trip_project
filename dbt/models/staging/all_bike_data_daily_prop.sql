74342c3a041908fe9380f4154471cd049d66f2ef
undersea-nate <131403349+undersea-nate@users.noreply.github.com>
2023-05-10 19:16:23 -0400

    Update all_bike_data_daily_prop.sql

--- dbt/models/staging/all_bike_data_daily_prop.sql
+++ dbt/models/staging/all_bike_data_daily_prop.sql
@@ -1,7 +1,16 @@
-create table  dbt_underseanate.all_bike_data_daily_prop as
-SELECT date, start_hour, sum(prop_duration) as prop_duration FROM
-(SELECT a.duration/b.sum_duration as prop_duration, a.date, a.start_hour, a.member_casual from `stately-transit-383518.dbt_underseanate.all_bike_data` as a,
-(SELECT date, sum(duration) as sum_duration FROM `stately-transit-383518.dbt_underseanate.all_bike_data` group by date) as b
+SELECT 
+  date, start_hour, sum(prop_duration) as prop_duration 
+FROM
+  (SELECT 
+    a.duration/b.sum_duration as prop_duration, a.date, a.start_hour, a.member_casual 
+  FROM
+    {{ source('dbt_underseanate', 'all_bike_data') }} 
+  as a,
+  (SELECT 
+    date, sum(duration) as sum_duration 
+   FROM 
+    {{ source('dbt_underseanate', 'all_bike_data') }} 
+   group by date) as b
 WHERE a.date = b.date)
-group by date, start_hour
-order by date, start_hour
+GROUP BY date, start_hour
+ORDER BY date, start_hour
